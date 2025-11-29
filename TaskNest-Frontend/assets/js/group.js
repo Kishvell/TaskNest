215e5d20502d28b316604eff4e7f21713e4093fb
Joash Tantiangco <tan24001@byui.edu>
2025-11-30 03:37:23 +0800

    Created model for group work

---
+++ TaskNest-Frontend/assets/js/group.js
@@ -0,0 +1,22 @@
+const contributorDiv = document.querySelector("#contributor");
+const contributorAdd = document.querySelector("#addContributor");
+const taskList = document.querySelector("#taskList");
+const addColumn = document.querySelector("#addColumn");
+const addContributorButton = document.querySelector("#addContributorButton");
+const addColumnButton = document.querySelector("#addColumnButton");
+
+contributorAdd.addEventListener("click", () => {
+    //show modal for adding contributors
+});
+
+addColumn.addEventListener("click", () => {
+    //show modal for adding columns
+})
+
+addContributorButton.addEventListener("click", () => {
+    //adds user for the project
+})
+
+addColumnButton.addEventListener("click", () => {
+    //adds column in the project
+})
